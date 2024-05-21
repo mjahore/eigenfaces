1bf0ee93080d86413ebcd4fedea41c917200a8fe
Mike Hore <hore@case.edu>
2024-05-21 13:15:28 -0400

    Add eigenvalues/variance/cum. sum

--- eigenface.py
+++ eigenface.py
@@ -10,8 +10,8 @@ from PIL import Image
 # Create our data matrix... we assume from the beginning that there
 # are only 25 faces total. We also assume the dimensions of each face
 # will be 172 x 180. That's my choice. You can make your own.
-h = 180
-w = 172
+h = 90
+w = 86
 
 # Read in each of the images from file, and resize.
 X = np.zeros((1,h*w))
@@ -52,6 +52,7 @@ plt.imsave('avg_face.png', A, cmap='gray')
 C = np.matmul(X.T,X)
 
 # Get eigenvectors and eigenvalues.
+print('Performing the SVD...')
 U,S,V = np.linalg.svd(C,full_matrices=1)
 
 # Get the sum of the eigenvalues to compute the %variance captured.
@@ -59,11 +60,19 @@ S_sum = 0.0
 for i in range(S.shape[0]):
 	S_sum = S_sum + S[i]
 
-# Print out the first 5 eigenfaces.
+# Print out the eigenfaces.
 for i in range(U.shape[1]):
 	if (S[i] > 1e-2):
 		eig_face = U[:,i].reshape((h,w), order='F')
 		print("Writing eigenvector " + str(i) + " with eigenvalue " + str(S[i]) + " (%var: " + str(S[i]/S_sum) + ")")
 		plt.imsave(str(i) + "_eig.png", eig_face, cmap='gray')
 
+
+# Print out the singular values.
+S = S / S_sum
+C_sum = 0.0
+for i in range(S.shape[0]):
+    C_sum = C_sum + S[i]
+    if (S[i] > 1e-2):
+        print(str(i) + " " + str(S[i]) + " " + str(C_sum))
 
